78c9e8960cd7f5896a74257a9e43bfc17f4bb5d5
Hilde Aas Nøst <hilde.nost@gmail.com>
2018-06-27 00:56:08 +0200

    Update cli: cannot run folder immediately after creating.

--- uintahtools/cli.py
+++ uintahtools/cli.py
@@ -12,6 +12,7 @@ from uintahtools import UPS, Suite, CONFIG, CONFIGSTR, udaplot
 readline.set_completer_delims(" \t\n")
 readline.parse_and_bind("tab: complete")
 
+
 def initialize_config():
     """Initializes the configuration file if it is not set.
 
@@ -31,7 +32,7 @@ def initialize_config():
             suggestion = os.path.expanduser(
                 input("UINTAHPATH is not set. Where is the Uintah executable?\t")
             )
-        
+
             # Verify that the input path is valid and existing before setting
             # Also expand the path so it is absolute before storing
             try:
@@ -43,10 +44,12 @@ def initialize_config():
                 break
         yaml.dump(settings, p)
 
+
 @click.group()
 def cli():
     """Command line scripts that simplify working with Uintah ups input files."""
     initialize_config()
+
 
 @cli.command("plot", short_help="plot two variables")
 @click.argument("xvar")
@@ -58,23 +61,30 @@ def plot(xvar, yvar, uda):
     udaplot(xvar, yvar, uda)
     click.echo("Finished!")
 
+
 @cli.command("generate", short_help="generate simulation suite")
 @click.argument("ups", type=click.File())
 @click.argument("yaml", type=click.File())
 @click.option("--run", is_flag=True, help="run all generated input files")
 def generate(ups, yaml, run):
     """Generate simulation suite based on a UPS file with settings from a YAML file."""
+    print("Generating...")
     folder = UPS(ups, yaml).generate_ups()
+    print("Generate simulation suite in", folder)
     if run:
-        folder_run(os.getcwd(), d=False)
-    
+        print("The automatic run feature is not implemented yet.")
+        # folder_run(folder)  # , d=False)
+
+
 @cli.command("run", short_help="run the simulation suite")
 @click.argument("folder")
 # @click.option('-d', is_flag=True, help="use dbg executable in stead of opt")
-def folder_run(folder):#, d):
+def folder_run(folder):  # , d):
     """Run all Uintah input files residing in FOLDER."""
-    click.echo("Running all ups files in folder {folder}".format(folder=folder))
+    click.echo(
+        "Running all ups files in folder {folder}".format(folder=folder))
+    print("Running...")
     # if d:
-        # click.echo("Using the debug executable.")
+    # click.echo("Using the debug executable.")
     suite = Suite(folder)
     suite.run()
